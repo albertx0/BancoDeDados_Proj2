2949740ef3696dd698961ff614f67b7153f8fb7f
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-04-29 20:45:22 -0300

    Update gerarDados.py

--- gerarDados.py
+++ gerarDados.py
@@ -21,11 +21,13 @@ qtd_series = 5
 
 ID_PRODUTORAS = set()
 ID_FUNCIONARIOS = set()
-ID_SERIE = set()
+ID_SERIES = set()
+ID_TEMPORADAS = set()
+ID_EPISODIOS = set()
 
 NOMES_PRODUTORAS = set()
 NOMES_FUNCIONARIOS = set()
-
+NOMES_SERIES = set()
 # /////////////////////
 
 # Listas que possuem dados estaticos
@@ -112,6 +114,31 @@ def gerar_nome_funcionario():
 
         return nome
 
+# funcao para gerar 
+# nomes de series
+def gerar_nome_serie():
+    while 1:
+        nome = faker.catch_phrase()
+
+        if nome in NOMES_SERIES:
+            continue
+
+        NOMES_SERIES.add(nome)
+
+        return nome
+
+# funcao para gerar
+# ids para series
+def gerar_id_series():
+    while 1:
+        ID = faker.bothify("??###?")
+
+        if ID in ID_SERIES:
+            continue
+
+        ID_SERIES.add(ID)
+
+        return ID
 def gerar_salario():
     return round(random.uniform(1500, 20000), 2)
 
@@ -160,10 +187,42 @@ def gerar_atores(produtoras):
             })
     return atores
 
+def diretor_aleatorio(diretores):
+    return faker.random_element(diretores)
+
+def restricao_aleatoria():
+    return faker.random_element(restricao_idade)
+
+def genero_aleatorio():
+    return faker.random_element(generos_series)
+
+def gerar_series(diretores):
+    series = []
+
+    for i in range(qtd_series):
+        diretor = diretor_aleatorio(diretores)
+
+        series.append({
+            "id_serie" : gerar_id_series(),
+            "nome" : gerar_nome_serie(),
+            "restricao_idade" : restricao_aleatoria(),
+            "genero" : genero_aleatorio(),
+            "qtd_temporadas" : random.randint(1, 5),
+            "id_prod" : diretor["id_prod"],
+            "id_diretor" : diretor["id_diretor"]
+            })
+    return series
+
+def gerar_temporadas(series):
+    temporadas = []
+
+    for i in range(len(series)):
+        qtd = random.randint(1, 5)
+
 
 if __name__ == "__main__":
     produtoras = gerar_produtoras()
     diretores = gerar_diretores(produtoras)
     atores = gerar_atores(produtoras)
-
-    print(atores)
+    series = gerar_series(diretores)
+    print(series)
