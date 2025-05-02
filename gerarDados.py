70d3dddc9a45bef159e2f6a6e9b2bdf56d59f578
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-05-02 18:58:54 -0300

    Update gerarDados.py

--- gerarDados.py
+++ gerarDados.py
@@ -28,6 +28,7 @@ ID_EPISODIOS = set()
 NOMES_PRODUTORAS = set()
 NOMES_FUNCIONARIOS = set()
 NOMES_SERIES = set()
+NOMES_EPISODIOS = set()
 # /////////////////////
 
 # Listas que possuem dados estaticos
@@ -62,6 +63,14 @@ restricao_idade = [
     "A16",
     "A18"
 ]
+
+# /////////////////////
+
+# dicionario de lista para ter armazenado os atores 
+# presentes em cada produtora
+atores_por_prod = {
+
+}
 
 # /////////////////////
 
@@ -127,6 +136,19 @@ def gerar_nome_serie():
 
         return nome
 
+# funcao para gerar 
+# nomes dos episodios 
+def gerar_nome_episodio():
+    while 1:
+        nome = faker.catch_phrase()
+
+        if nome in NOMES_EPISODIOS:
+            continue
+
+        NOMES_SERIES.add(nome)
+
+        return nome
+
 # funcao para gerar
 # ids para series
 def gerar_id_series():
@@ -139,6 +161,29 @@ def gerar_id_series():
         ID_SERIES.add(ID)
 
         return ID
+
+def gerar_id_temporadas():
+    while 1:
+        ID = faker.bothify("??###")
+
+        if ID in ID_TEMPORADAS:
+            continue
+        
+        ID_TEMPORADAS.add(ID)
+
+        return ID
+
+def gerar_id_episodios():
+    while 1:
+        ID = faker.bothify("???##")
+
+        if ID in ID_EPISODIOS:
+            continue
+        
+        ID_EPISODIOS.add(ID)
+
+        return ID
+
 def gerar_salario():
     return round(random.uniform(1500, 20000), 2)
 
@@ -152,11 +197,14 @@ def gerar_produtoras():
     produtoras = []
 
     for i in range(qtd_produtoras):
+        id_prod = gerar_id_produtora()
+
         produtoras.append({
-            "id_prod" : gerar_id_produtora(),
+            "id_prod" : id_prod,
             "nome_produtora" : gerar_nome_produtora(),
             "CEO" : gerar_nome_funcionario()
             })
+        atores_por_prod[id_prod] = []
     return produtoras
 
 def id_produtora_aleatoria(produtoras):
@@ -179,12 +227,17 @@ def gerar_atores(produtoras):
     atores = []
 
     for i in range(qtd_atores):
+        id_prod = id_produtora_aleatoria(produtoras)
+        id_ator = gerar_id_funcionario()
+
         atores.append({
-            "id_ator" : gerar_id_funcionario(),
+            "id_ator" : id_ator,
             "nome" : gerar_nome_funcionario(),
             "salario" : gerar_salario(),
-            "id_prod" : id_produtora_aleatoria(produtoras)
+            "id_prod" : id_prod
             })
+        
+        atores_por_prod[id_prod].append(id_ator)
     return atores
 
 def diretor_aleatorio(diretores):
@@ -216,13 +269,65 @@ def gerar_series(diretores):
 def gerar_temporadas(series):
     temporadas = []
 
-    for i in range(len(series)):
-        qtd = random.randint(1, 5)
+    for serie in series:
+        
+        for i in range(serie["qtd_temporadas"]): 
+            ID = gerar_id_temporadas()
+            id_prod = serie["id_prod"]
 
+            temporadas.append({
+                "id_temp" : ID,
+                "qtd_ep" : random.randint(1, 12), 
+                "nota_avaliacao" : round(random.uniform(0, 10), 2),
+                "numero_temp" : i+1,
+                "id_serie" : id_prod,
+                "id_ator" : faker.random_element(atores_por_prod[id_prod])
+                 })
+    return temporadas
+
+def gerar_episodios(temporadas):
+    episodios = []
+
+    for temporada in temporadas:
+        id_temp = temporada["id_temp"]
+        for i in range(temporada["qtd_ep"]):
+            ID = gerar_id_episodios()
+            titulo = gerar_nome_episodio()
+        episodios.append({
+            "id_ep" : ID, 
+            "titulo" : titulo,
+            "duracao" : random.randint(20, 100),
+            "nota_avaliacao" : round(random.uniform(0, 10), 2),
+            "id_temp" : id_temp
+            })
+    return episodios
+
+def exportar_csv(nome_arquivo, lista_dicionarios):
+    if not lista_dicionarios:
+        print(f"[!] Lista vazia, não exportou: {nome_arquivo}")
+        return
+
+    os.makedirs("DadosCsv", exist_ok=True)
+
+    caminho = os.path.join("DadosCsv", nome_arquivo)
+
+    with open(caminho, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=lista_dicionarios[0].keys())
+        writer.writeheader()
+        writer.writerows(lista_dicionarios)
+    print(f"[✓] Exportado: {nome_arquivo}")
 
 if __name__ == "__main__":
     produtoras = gerar_produtoras()
     diretores = gerar_diretores(produtoras)
     atores = gerar_atores(produtoras)
     series = gerar_series(diretores)
-    print(series)
+    temporadas = gerar_temporadas(series)
+    episodios = gerar_episodios(temporadas)
+    
+    exportar_csv("Produtora", produtoras)
+    exportar_csv("Diretor", diretores)
+    exportar_csv("Ator", atores)
+    exportar_csv("Serie", series)
+    exportar_csv("Temporada", temporadas)
+    exportar_csv("Episodio", episodios)
